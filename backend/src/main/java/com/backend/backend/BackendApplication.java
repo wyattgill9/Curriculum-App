e16a547b0d53f7a92c97f5c3cbf9a7ff632526e3
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-12-09 18:47:06 -0800

    ci/cd test

--- backend/src/main/java/com/backend/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/backend/BackendApplication.java
@@ -2,7 +2,7 @@ package com.backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class BackendApplication {
 
