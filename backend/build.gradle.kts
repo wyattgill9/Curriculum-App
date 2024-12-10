a1ec71bdcc0bd4b55493e27f4bd8f4fb614c6891
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-12-09 18:39:36 -0800

    Hello World

---
+++ backend/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.backend"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
